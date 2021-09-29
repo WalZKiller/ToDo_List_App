19cdb2f7de58001013f9217c9fcfc3358222b7f1
WalZKiller <walidwajdy@gmail.com>
2021-09-29 13:08:22 +0800

    first commit, created global style

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+//Global Style
+import GlobalStyle from './GlobalStyle';
+
+//Components
+
+function App() {
+
+  return (
+    <>
+      <GlobalStyle />
+      yy
+    </>
+  );
+}
+
+export default App;
---
+++ src/GlobalStyle.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        background: red;
+    };
+
+    html {
+        margin: 0;
+        padding: 0;
+    }
+`;
+
+export default GlobalStyle;
